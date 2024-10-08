64aafb8f299dc26e5b3735c32c64cdb505d7623b
Ali Eriş <v9zz@hotmail.com>
2024-10-08 23:55:10 +0300

    yine adam akıllı bir şey anlatılmadı

---
+++ programlama/bir.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+//#include <conio.h>     //ben linuxta derlediğimden vscode ->wsl -> ubuntu -> gcc haliyle no conio no cry
+
+void  main()
+{
+int s1,s2,toplam;
+scanf("%d%d",&s1,&s2);
+toplam = s1 + s2;
+printf("%d",toplam);
+}
+
+
+
+/*
+
+sabah struct reis yargı dağıtıyor şimdi çocuk parkında gibiyim
+
+
+*/
