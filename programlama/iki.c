9582eb773ad0e52612905131af128d5d3c6edbdf
Ali Eriş <v9zz@hotmail.com>
2024-10-09 20:43:30 +0300

    üzüldük

---
+++ programlama/iki.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+
+int main()
+{
+
+// ne oldu bu sene c ve java senesi //
+
+
+
+int i,j,top;
+
+scanf("%d%d",&i,&j);
+//for(int i=100;i>0;i--)
+
+top = i + j;
+printf("%d %d %d",i,j,top);
+
+
+
+
+}
+
+
+//diğer hocanın değerini bilemedim be
+
+// üf yaptığımız örneğe baq be 
+//ъа нфзешхшьшя жуну ифл иу щкщызгтгт умдфвш
+
+           
---
+++ programlama/uc.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+
+int main()
+{
+int i,j, top;
+printf("iki sayi giriniz");
+
+scanf("%d %d",&i,&j);
+
+top = i + j;
+for(int k=5;k>0;k--)
+{
+printf("%d %d %d \n",i,j,top);
+
+}
+
+//ben yine şovunu yapar bir şey gösterir dedim be burada bıraktı bizi neyse bir şey demiyorum
+//en zevkli olabilecek ders piç oldu tşkkrler sprmn
+}
